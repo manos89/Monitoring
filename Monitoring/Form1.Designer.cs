c32fb72e86f415b71fb4168ae69a56eedae24c3e
manos Vahlas <manos_vahlas@hotmail.com>
2018-08-09 00:58:40 +0300

    Added Followers Difference

--- Monitoring/Form1.Designer.cs
+++ Monitoring/Form1.Designer.cs
@@ -31,25 +31,30 @@
             this.usrnmbox = new System.Windows.Forms.TextBox();
             this.passwdbox = new System.Windows.Forms.TextBox();
             this.submitbttn = new System.Windows.Forms.Button();
-            this.fllowerslbl = new System.Windows.Forms.Label();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
-            this.label4 = new System.Windows.Forms.Label();
-            this.followingslbl = new System.Windows.Forms.Label();
-            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.delaybox = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.usrsfiledialog = new System.Windows.Forms.OpenFileDialog();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.usrchcklbl = new System.Windows.Forms.Label();
+            this.filelbl = new System.Windows.Forms.Label();
+            this.loglbl = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
             // usrnmbox
             // 
-            this.usrnmbox.Location = new System.Drawing.Point(296, 150);
+            this.usrnmbox.Location = new System.Drawing.Point(232, 74);
             this.usrnmbox.Name = "usrnmbox";
             this.usrnmbox.Size = new System.Drawing.Size(246, 20);
             this.usrnmbox.TabIndex = 0;
             // 
             // passwdbox
             // 
-            this.passwdbox.Location = new System.Drawing.Point(296, 225);
+            this.passwdbox.Location = new System.Drawing.Point(232, 134);
             this.passwdbox.Name = "passwdbox";
             this.passwdbox.PasswordChar = '*';
             this.passwdbox.Size = new System.Drawing.Size(246, 20);
@@ -57,7 +62,7 @@
             // 
             // submitbttn
             // 
-            this.submitbttn.Location = new System.Drawing.Point(375, 329);
+            this.submitbttn.Location = new System.Drawing.Point(232, 343);
             this.submitbttn.Name = "submitbttn";
             this.submitbttn.Size = new System.Drawing.Size(75, 23);
             this.submitbttn.TabIndex = 2;
@@ -65,20 +70,11 @@
             this.submitbttn.UseVisualStyleBackColor = true;
             this.submitbttn.Click += new System.EventHandler(this.submitbttn_Click);
             // 
-            // fllowerslbl
-            // 
-            this.fllowerslbl.AutoSize = true;
-            this.fllowerslbl.Location = new System.Drawing.Point(763, 157);
-            this.fllowerslbl.Name = "fllowerslbl";
-            this.fllowerslbl.Size = new System.Drawing.Size(13, 13);
-            this.fllowerslbl.TabIndex = 3;
-            this.fllowerslbl.Text = "0";
-            // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(161)));
-            this.label1.Location = new System.Drawing.Point(183, 153);
+            this.label1.Location = new System.Drawing.Point(104, 77);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(76, 13);
             this.label1.TabIndex = 4;
@@ -88,71 +84,109 @@
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(161)));
-            this.label2.Location = new System.Drawing.Point(181, 232);
+            this.label2.Location = new System.Drawing.Point(102, 137);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(78, 13);
             this.label2.TabIndex = 5;
             this.label2.Text = "PASSWORD";
             // 
-            // label3
+            // label5
             // 
-            this.label3.AutoSize = true;
-            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(161)));
-            this.label3.Location = new System.Drawing.Point(670, 157);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(87, 13);
-            this.label3.TabIndex = 6;
-            this.label3.Text = "FOLLOWERS:";
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(161)));
+            this.label5.Location = new System.Drawing.Point(45, 196);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(135, 13);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "DELAY (IN SECONDS)";
             // 
-            // label4
+            // label6
             // 
-            this.label4.AutoSize = true;
-            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(161)));
-            this.label4.Location = new System.Drawing.Point(670, 196);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(92, 13);
-            this.label4.TabIndex = 7;
-            this.label4.Text = "FOLLOWINGS:";
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(161)));
+            this.label6.Location = new System.Drawing.Point(102, 256);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(79, 13);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "USERS FILE";
             // 
-            // followingslbl
+            // delaybox
             // 
-            this.followingslbl.AutoSize = true;
-            this.followingslbl.Location = new System.Drawing.Point(766, 195);
-            this.followingslbl.Name = "followingslbl";
-            this.followingslbl.Size = new System.Drawing.Size(13, 13);
-            this.followingslbl.TabIndex = 8;
-            this.followingslbl.Text = "0";
+            this.delaybox.Location = new System.Drawing.Point(232, 192);
+            this.delaybox.Name = "delaybox";
+            this.delaybox.Size = new System.Drawing.Size(246, 20);
+            this.delaybox.TabIndex = 12;
             // 
-            // tableLayoutPanel1
+            // button1
             // 
-            this.tableLayoutPanel1.ColumnCount = 2;
-            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
-            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
-            this.tableLayoutPanel1.Location = new System.Drawing.Point(617, 343);
-            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
-            this.tableLayoutPanel1.RowCount = 2;
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
-            this.tableLayoutPanel1.Size = new System.Drawing.Size(200, 100);
-            this.tableLayoutPanel1.TabIndex = 9;
+            this.button1.Location = new System.Drawing.Point(232, 250);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(80, 24);
+            this.button1.TabIndex = 13;
+            this.button1.Text = "Select...";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // usrsfiledialog
+            // 
+            this.usrsfiledialog.FileName = "openFileDialog1";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(498, 97);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(674, 574);
+            this.dataGridView1.TabIndex = 14;
+            // 
+            // usrchcklbl
+            // 
+            this.usrchcklbl.AutoSize = true;
+            this.usrchcklbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.usrchcklbl.Location = new System.Drawing.Point(639, 49);
+            this.usrchcklbl.Name = "usrchcklbl";
+            this.usrchcklbl.Size = new System.Drawing.Size(0, 13);
+            this.usrchcklbl.TabIndex = 15;
+            // 
+            // filelbl
+            // 
+            this.filelbl.AutoSize = true;
+            this.filelbl.Location = new System.Drawing.Point(333, 250);
+            this.filelbl.MaximumSize = new System.Drawing.Size(140, 0);
+            this.filelbl.Name = "filelbl";
+            this.filelbl.Size = new System.Drawing.Size(0, 13);
+            this.filelbl.TabIndex = 16;
+            // 
+            // loglbl
+            // 
+            this.loglbl.AutoSize = true;
+            this.loglbl.Location = new System.Drawing.Point(232, 407);
+            this.loglbl.Name = "loglbl";
+            this.loglbl.Size = new System.Drawing.Size(0, 13);
+            this.loglbl.TabIndex = 17;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(872, 496);
-            this.Controls.Add(this.tableLayoutPanel1);
-            this.Controls.Add(this.followingslbl);
-            this.Controls.Add(this.label4);
-            this.Controls.Add(this.label3);
+            this.ClientSize = new System.Drawing.Size(1184, 761);
+            this.Controls.Add(this.loglbl);
+            this.Controls.Add(this.filelbl);
+            this.Controls.Add(this.usrchcklbl);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.delaybox);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.fllowerslbl);
             this.Controls.Add(this.submitbttn);
             this.Controls.Add(this.passwdbox);
             this.Controls.Add(this.usrnmbox);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Instagram Monitoring";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -163,13 +197,17 @@
         private System.Windows.Forms.TextBox usrnmbox;
         private System.Windows.Forms.TextBox passwdbox;
         private System.Windows.Forms.Button submitbttn;
-        private System.Windows.Forms.Label fllowerslbl;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.Label label4;
-        private System.Windows.Forms.Label followingslbl;
-        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox delaybox;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.OpenFileDialog usrsfiledialog;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label usrchcklbl;
+        private System.Windows.Forms.Label filelbl;
+        private System.Windows.Forms.Label loglbl;
     }
 }
 
--- Monitoring/Form1.cs
+++ Monitoring/Form1.cs
@@ -4,6 +4,7 @@ using InstaSharper.Classes;
 using InstaSharper.Classes.Models;
 using InstaSharper.Logger;
 using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -17,6 +18,9 @@ namespace Monitoring
 {
     public partial class Form1 : Form
     {
+        ArrayList userlist = new ArrayList();
+        IDictionary<string, int> startingusers = new Dictionary<string, int>();
+
         public Form1()
         {
             InitializeComponent();
@@ -33,23 +37,109 @@ namespace Monitoring
                 UserName = UserName,
                 Password = Password
             };
-           
+            DataTable dt = new DataTable();
+            dt.Columns.Add("Username");
+            dt.Columns.Add("Profile Photo");
+            dt.Columns.Add("Followers");
+            dt.Columns.Add("Followings");
+            dt.Columns.Add("Mutual Followers");
+            dt.Columns.Add("Followers Difference");
+            dataGridView1.DataSource = dt;
             var api = InstaApiBuilder.CreateBuilder()
         .SetUser(userSession)
         .UseLogger(new DebugLogger(LogLevel.Exceptions))
-        .SetRequestDelay(new TimeSpan(0, 0, 30))
+        .SetRequestDelay(new TimeSpan(0, 0, Int32.Parse(delaybox.Text.ToString())))
         .Build();
 
             IResult<InstaLoginResult> loggedIn = await api.LoginAsync();
+            if(loggedIn.Value.ToString()== "BadPassword")
+            {
+                MessageBox.Show("Failed to login");
+                Application.Exit();
+            }
+           
+            System.Diagnostics.Debug.WriteLine(loggedIn.Value.ToString());
+
+
 
             while (true)
             {
-                var Followers = await api.GetCurrentUserFollowersAsync(PaginationParameters.MaxPagesToLoad(1));
-                var Followings = await api.GetUserFollowingAsync("manosv_11", PaginationParameters.MaxPagesToLoad(1));
-                fllowerslbl.Text = Followers.Value.Count.ToString();
-                followingslbl.Text = Followings.Value.Count.ToString();
+                foreach (string user in userlist)
+                {
+                    try
+                    {
+                        usrchcklbl.Text = "Checking User: " + user.ToString();
+                        var followingsdata = await api.GetUserFollowingAsync(user, PaginationParameters.MaxPagesToLoad(1));
+                        var Userinfo = await api.GetUserAsync(user);
+                        string Username = Userinfo.Value.UserName.ToString();
+                        string Profilephoto = Userinfo.Value.ProfilePicture.ToString();
+                        string Followers = Userinfo.Value.FollowersCount.ToString();
+                        string Followings = followingsdata.Value.Count.ToString();
+                        string MutualFollowers = Userinfo.Value.MutualFollowers.ToString();
+  
+                        DataRow[] foundAuthors = dt.Select("Username = '" + Username + "'");
+                        if (foundAuthors.Length == 0)
+                        {
+                            startingusers.Add(new KeyValuePair<string, int>(Username, Int32.Parse(Followers)));
+                            dt.Rows.Add(Username, Profilephoto, Followers, Followings, MutualFollowers, "0");
+                        }
+                        else
+                        {
+                            int Difference = Int32.Parse(Followers) - startingusers[Username];
+                            DataRow dr = dt.Select("Username='" + Username + "'").FirstOrDefault();
+                            dr["Profile Photo"] = Profilephoto;
+                            dr["Followers"] = Followers;
+                            dr["Followings"] = Followings;
+                            dr["Mutual Followers"] = MutualFollowers;
+                            dr["Followers Difference"] = Difference;
+
+                        }
+                        dataGridView1.DataSource = dt;
+
+                    }
+
+                    catch
+                    {
+                        
+                    }
+                }
             }
 
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void openFileDialog1_FileOk(object sender, CancelEventArgs e)
+        {
+
+        }
+
+        private void tableLayoutPanel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string line;
+            int counter = 0;
+            usrsfiledialog.Filter= "Text|*.txt|All|*.*";
+            if (usrsfiledialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                filelbl.Text = usrsfiledialog.FileName.ToString();
+                System.IO.StreamReader file = new System.IO.StreamReader(usrsfiledialog.FileName);
+                while ((line = file.ReadLine()) != null)
+                {
+                    userlist.Add(line.ToString());
+                    counter++;
+                }
+
+
+                file.Close();
+            }
         }
     }
 }
